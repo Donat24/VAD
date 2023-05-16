0741e0f492594772f566b7d9425d2257caa5a6d7
Donat24 <45335473+Donat24@users.noreply.github.com>
2023-05-16 13:36:53 +0200

    TAR update

---
+++ Scripte/data/generation/Steps/7_pack_tar.py
@@ -0,0 +1,21 @@
+import os
+import tarfile
+
+#PARAMETER
+TAR_TRAIN = "train.tar"
+DIR_TRAIN = "./DATA/TRAIN"
+
+TAR_TEST = "test.tar"
+DIR_TEST = "./DATA/TEST"
+
+#Erzeugt Tar
+for tar_file, dir in [(TAR_TRAIN, DIR_TRAIN), (TAR_TEST, DIR_TEST)]:
+
+    #Löscht altes Tar
+    if os.path.exists(tar_file):
+        os.remove(tar_file)
+
+    with tarfile.open(tar_file, mode="w") as tar:
+
+        for file in os.listdir(dir):
+            tar.add( name = file )
