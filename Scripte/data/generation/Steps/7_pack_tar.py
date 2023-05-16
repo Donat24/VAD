8795045e623fa08a4baeb2e0eebeff729a10b465
Donat24 <45335473+Donat24@users.noreply.github.com>
2023-05-16 13:51:39 +0200

    Update 7_pack_tar.py

--- Scripte/data/generation/Steps/7_pack_tar.py
+++ Scripte/data/generation/Steps/7_pack_tar.py
@@ -2,11 +2,11 @@ import os
 import tarfile
 
 #PARAMETER
-TAR_TRAIN = "train.tar"
-DIR_TRAIN = "./DATA/TRAIN"
+TAR_TRAIN = r"/mnt/data/source_jonas/VAD/Scripte/data/samples/DATA/train.tar"
+DIR_TRAIN = r"/mnt/data/source_jonas/VAD/Scripte/data/samples/DATA/TRAIN"
 
-TAR_TEST = "test.tar"
-DIR_TEST = "./DATA/TEST"
+TAR_TEST = r"/mnt/data/source_jonas/VAD/Scripte/data/samples/DATA/test.tar"
+DIR_TEST = r"/mnt/data/source_jonas/VAD/Scripte/data/samples/DATA/TEST"
 
 #Erzeugt Tar
 for tar_file, dir in [(TAR_TRAIN, DIR_TRAIN), (TAR_TEST, DIR_TEST)]:
@@ -18,4 +18,4 @@ for tar_file, dir in [(TAR_TRAIN, DIR_TRAIN), (TAR_TEST, DIR_TEST)]:
     with tarfile.open(tar_file, mode="w") as tar:
 
         for file in os.listdir(dir):
-            tar.add( name = file )
+            tar.add( name = os.path.join(dir, file), arcname=file)
