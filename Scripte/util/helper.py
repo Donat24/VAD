d527e057218e4a57c043539146120ec73723da79
Donat24 <45335473+Donat24@users.noreply.github.com>
2023-05-20 14:41:33 +0200

    Update helper.py

--- Scripte/util/helper.py
+++ Scripte/util/helper.py
@@ -1,13 +1,19 @@
 from data.data import *
+from .datasets import *
+
 from IPython.display import Audio
 from IPython.display import clear_output, display
 
-def get_audio(idxs):
+def get_audio(dataset, idxs):
     
+    #lädt UnchunkedDataset
+    if isinstance(dataset, ChunkedDataset):
+        dataset = dataset.dataset
+
     #Cast
     if isinstance(idxs, int):
         idxs = [idxs]
     #Iter
     for idx in idxs:
-        x,y = speakdataset_train_unchunked[idx]
-        display(Audio(data=x, rate=speakdataset_train_unchunked.dataset.target_samplerate))
+        x,y = dataset[idx]
+        display(Audio(data=x, rate=dataset.dataset.target_samplerate))
