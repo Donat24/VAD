4a03ed8af2e9287372f66c57d2071944a10ea112
Donat24 <45335473+Donat24@users.noreply.github.com>
2023-05-25 01:10:03 +0200

    Update data.py

--- Scripte/data/data.py
+++ Scripte/data/data.py
@@ -41,7 +41,7 @@ def get_y(tensor, sr ,info):
 #SpeakDataset
 speakdataset_train_unchunked = SpeakDataset(filedataset_train, audio_processing_chain = audio_processing_chain_tain, get_y = get_y)
 speakdataset_test_unchunked  = SpeakDataset(filedataset_test,  audio_processing_chain = audio_processing_chain_test, get_y = get_y)
-speakdataset_val_unchunked   = SpeakDataset(filedataset_train, audio_processing_chain = audio_processing_chain_test, get_y = get_y)
+speakdataset_val_unchunked   = SpeakDataset(filedataset_train, audio_processing_chain = audio_processing_chain_val,  get_y = get_y)
 
 #ChunkedDataset
 dataset_train = ChunkedDataset(speakdataset_train_unchunked, SAMPLE_LENGTH, HOP_LENGTH, CONTEXT_LENGTH, y_truth_treshold = TRUTH_TRESHOLD)
