c8f063295c680f6bfb3abe8247c0bfc30ee8739f
Donat24 <45335473+Donat24@users.noreply.github.com>
2023-05-25 01:04:14 +0200

    normalizer hinzugefügt

--- Scripte/data/audio_processing_chains.py
+++ Scripte/data/audio_processing_chains.py
@@ -11,23 +11,32 @@ class AudioProcessing(nn.Module):
 #Train
 class AudioProcessingTrain(AudioProcessing):
     
-    def __init__(self) -> None:
+    def __init__(self, normalizer = None) -> None:
         
         #Init
         super().__init__()
         
         #Fx
         self.random_gain = RandomGain()
+        self.normalizer = normalizer
     
     def forward(self, x, sr = None, info = None):
+        
         out = x
+        
+        #Random Gain
         out = self.random_gain(x)
+        
+        #Normalizer Forward
+        if self.normalizer is not None:
+            out = self.normalizer(out)
+        
         return out
 
 #Test
 class AudioProcessingTest(AudioProcessing):
     
-    def __init__(self) -> None:
+    def __init__(self, normalizer = None) -> None:
         
         #Init
         super().__init__()
@@ -36,6 +45,14 @@ class AudioProcessingTest(AudioProcessing):
         self.gain = Gain()
     
     def forward(self, x, sr = None, info = None):
+        
         out = x
+        
+        #Random Gain
         out = self.gain(x, gain = info["gain"])
+
+        #Normalizer Forward
+        if self.normalizer is not None:
+            out = self.normalizer(out)
+        
         return out
