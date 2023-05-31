1e7174a8a5688644ea87002ddce205d1c002064c
Donat24 <45335473+Donat24@users.noreply.github.com>
2023-05-31 12:18:58 +0200

    AVA

---
+++ Scripte/data/generation/ava/download.py
@@ -0,0 +1,19 @@
+import os
+import pandas as pd
+from pytube import YouTube
+
+#PARAMS
+path = r"D:\Masterarbeit\DATA GENERATION\AVA"
+
+#Erstellt Pfad
+if not os.path.exists(path):
+    os.mkdir(path)
+
+#Lädt CSV
+df = pd.read_csv("./ava_speech_labels_v1.csv", header=None)
+
+for id in df[0].unique():
+    yt = YouTube.from_id(id)
+    stream = yt.streams.filter(only_audio=True).filter(abr="128kbps")[0]
+    fieending = stream.mime_type.split("/")[-1]
+    stream.download(output_path = path, filename = f"{id}.{fieending}")
