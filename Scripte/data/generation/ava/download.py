d1cf712add8bf736080f752eadb000f11d1f07c3
Donat24 <45335473+Donat24@users.noreply.github.com>
2023-05-31 12:39:35 +0200

    Update download.py

--- Scripte/data/generation/ava/download.py
+++ Scripte/data/generation/ava/download.py
@@ -13,7 +13,11 @@ if not os.path.exists(path):
 df = pd.read_csv("./ava_speech_labels_v1.csv", header=None)
 
 for id in df[0].unique():
-    yt = YouTube(url=f"https://www.youtube.com/watch?v={id}")
-    stream = yt.streams.filter(only_audio=True).filter(abr="128kbps")[0]
-    fieending = stream.mime_type.split("/")[-1]
-    stream.download(output_path = path, filename = f"{id}.{fieending}")
+    try:
+        yt = YouTube(url=f"https://www.youtube.com/watch?v={id}")
+        stream = yt.streams.filter(only_audio=True).filter(abr="128kbps")[0]
+        fieending = stream.mime_type.split("/")[-1]
+        stream.download(output_path = path, filename = f"{id}.{fieending}")
+    
+    except:
+        print(f"Fehler bei ID{id}")
